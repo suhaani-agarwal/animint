0d6eeca886efa9403a00f8bf18caac7ffd90914a
Suhaani Agarwal <suhaani0707@gmail.com>
2025-05-10 13:23:36 +0530

    modified world bank paper data viz

--- inst/examples/WorldBank-paper.R
+++ inst/examples/WorldBank-paper.R
@@ -1,37 +1,50 @@
-library(animint)
+library(animint2)
 data(WorldBank)
-WorldBank$region <-
-  sub(" (all income levels)", "", WorldBank$region, fixed=TRUE)
-wb.paper <-
-  list(ts=ggplot()+
-       make_tallrect(WorldBank, "year")+
-       guides(color="none")+
-       geom_line(aes(year, life.expectancy, group=country, colour=region,
-                     showSelected=region,
-                     clickSelects=country),
-                 data=WorldBank, size=4, alpha=3/5),
-       scatter=ggplot()+
-       geom_point(aes(fertility.rate, life.expectancy, clickSelects=country,
-                      showSelected=year, colour=region, size=population,
-                      tooltip=paste(country, "population", population),
-                      key=country), # key aesthetic for animated transitions!
-                  data=WorldBank)+
-       geom_text(aes(fertility.rate, life.expectancy, label=country,
-                     clickSelects=country,
-                     showSelected=country, showSelected2=year,
-                     showSelected3=region,
-                     key=country), #also use key here!
-                 data=WorldBank)+
-       scale_size_animint(breaks=10^(9:5))+
-       make_text(WorldBank, 5, 80, "year"),
-       time=list(variable="year",ms=3000),
-       duration=list(year=1000),
-       selector.types=list(country="multiple", region="multiple"),
-       first=list(
-         year=1979,
-         country=c("United States", "Vietnam"),
-         region=c("East Asia & Pacific", "North America")
-         ),
-       title="World Bank data (Animint paper version)")
-animint2dir(wb.paper, "WorldBank-paper")
+WorldBank$region <- sub(" (all income levels)", "", WorldBank$region, fixed=TRUE)
+wb.paper <- animint(
+  title = "World Bank data viz for Animint paper",
+  source = "https://github.com/suhaani-agarwal/animint",
+  ts = ggplot() +
+    make_tallrect(WorldBank, "year") +
+    guides(color = "none") +
+    geom_line(
+      aes(year, life.expectancy, group = country, colour = region),
+      data = WorldBank,
+      size = 4,
+      alpha = 3/5,
+      showSelected = "region",
+      clickSelects = "country"
+    ),
+  scatter = ggplot() +
+    geom_point(
+      aes(fertility.rate, life.expectancy, colour = region, size = population),
+      data = WorldBank,
+      tooltip = paste(WorldBank$country, "population", WorldBank$population),
+      key = "country",
+      clickSelects = "country",
+      showSelected = "year"
+    ) +
+    geom_text(
+      aes(fertility.rate, life.expectancy, label = country),
+      data = WorldBank,
+      key = "country",
+      clickSelects = "country",
+      showSelected = c("country", "year", "region")
+    ) +
+    scale_size_animint(breaks = 10^(9:5)) +
+    make_text(WorldBank, 5, 80, "year"),
+  
+  time = list(variable = "year", ms = 3000),
+  duration = list(year = 1000),
+  selector.types = list(
+    country = "multiple",
+    region = "multiple"
+  ),
+  first = list(
+    year = 1979,
+    country = c("United States", "Vietnam"),
+    region = c("East Asia & Pacific", "North America")
+  )
+)
 
+animint2pages(wb.paper, "WorldBank-paper")
